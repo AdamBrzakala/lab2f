d09e9d575f9b46e8d8be4b319eef085378230d67
student <student@L407-10.carbon.cti.p.lodz.pl>
2022-05-23 10:54:03 +0200

    Update

---
+++ db.js
@@ -0,0 +1,194 @@
+let db;
+
+window.onload = () => {
+    var request = indexedDB.open('clientData', 1);
+
+    request.onupgradeneeded = function (e) {
+
+        db = e.target.result;
+
+        if (!db.objectStoreNames.contains('user')) {
+
+            var objectStore = db.createObjectStore('user', { keyPath: 'id' });
+            objectStore.createIndex('id', 'id', { unique: false });
+            objectStore.createIndex('email', 'email', { unique: false });
+            objectStore.createIndex('zipcode', 'zipcode', { unique: false });
+            objectStore.createIndex('nip', 'nip', { unique: false });
+            objectStore.createIndex('dowod', 'dowod', { unique: false });
+            objectStore.createIndex('ip4', 'ip4', { unique: false });
+            objectStore.createIndex('www', 'www', { unique: false });
+            objectStore.createIndex('diskA', 'diskA', { unique: false });
+            objectStore.createIndex('diskB', 'diskB', { unique: false });
+            objectStore.createIndex('ip6', 'ip6', { unique: false });
+            objectStore.createIndex('catalog', 'catalog', { unique: false });
+            objectStore.createIndex('phone', 'phone', { unique: false });
+        }
+    }
+
+    request.onsuccess = function (e) {
+        console.log('Database opened!');
+        db = e.target.result;
+        loadData();
+    }
+
+    request.onerror = function (e) {
+        console.log('Database opening error!');
+    }
+};
+
+function remove(elem) {
+    elem.parentElement.remove();
+}
+
+function loadData() {
+    const request = db.transaction('user').objectStore('user').getAll();
+
+    request.onsuccess = () => {
+        document.getElementById('klienci').innerHTML = JSON.stringify(request.result);
+    }
+
+    request.onerror = (err) => {
+        console.error(`Error on loadData() function`)
+    }
+}
+
+function saveData() {
+    var newUser = getclientData();
+    const transaction = db.transaction(['user'], 'readwrite');
+    const objectStore = transaction.objectStore('user');
+    const request = objectStore.put(newUser);
+    request.onsuccess = event => {
+        loadData();
+    };
+}
+
+function exampleText() {
+    var email = document.getElementById('email');
+    var zipcode = document.getElementById('zipcode');
+    var nip = document.getElementById('nip');
+    var dowod = document.getElementById('dowod');
+    var ip4 = document.getElementById('ip4');
+    var www = document.getElementById('www');
+    var diskA = document.getElementById('diskA');
+    var diskB = document.getElementById('diskB');
+    var ip6 = document.getElementById('ip6');
+    var catalog = document.getElementById('catalog');
+    var phone = document.getElementById('phone');
+    email.value = "raz@gmail.com";
+    zipcode.value = "91-112";
+    nip.value = "100-10-20-200";
+    dowod.value = "ABC 123456";
+    ip4.value = "192.168.1.1";
+    www.value = "www.google.com";
+    diskA.value = 'c:\\windows\\temp';
+    diskB.value = "C:\\Windows\\temp";
+    catalog.value = "/etc/passwd";
+    ip6.value = "2001:0db8:85a3:0000:0000:8a2e:0370:7334";
+    phone.value = "492 492 492";
+}
+
+function clearText() {
+    var email = document.getElementById('email');
+    var zipcode = document.getElementById('zipcode');
+    var nip = document.getElementById('nip');
+    var dowod = document.getElementById('dowod');
+    var ip4 = document.getElementById('ip4');
+    var www = document.getElementById('www');
+    var diskA = document.getElementById('diskA');
+    var diskB = document.getElementById('diskB');
+    var ip6 = document.getElementById('ip6');
+    var catalog = document.getElementById('catalog');
+    var phone = document.getElementById('phone');
+    email.value = "";
+    zipcode.value = "";
+    nip.value = "";
+    dowod.value = "";
+    ip4.value = "";
+    www.value = "";
+    diskA.value = "";
+    diskB.value = "";
+    catalog.value = "";
+    ip6.value = "";
+    phone.value = "";
+
+    var request = db.transaction(["user"], "readwrite")
+        .objectStore("user")
+        .clear();
+
+    request.onsuccess = event => {
+        loadData();
+    };
+
+    document.getElementById('wyszukiwanie').innerHTML = "";
+}
+
+function getclientData() {
+
+    var email = document.getElementById('email').value;
+    var zipcode = document.getElementById('zipcode').value;
+    var nip = document.getElementById('nip').value;
+    var dowod = document.getElementById('dowod').value;
+    var ip4 = document.getElementById('ip4').value;
+    var www = document.getElementById('www').value;
+    var diskA = document.getElementById('diskA').value;
+    var diskB = document.getElementById('diskB').value;
+    var ip6 = document.getElementById('ip6').value;
+    var catalog = document.getElementById('catalog').value;
+    var phone = document.getElementById('phone').value;
+
+    return {
+        id: new Date().getTime(),
+        email: email,
+        zipcode: zipcode,
+        ip4: ip4,
+        nip: nip,
+        dowod: dowod,
+        www: www,
+        diskA: diskA,
+        diskB: diskB,
+        ip6: ip6,
+        catalog: catalog,
+        phone: phone
+    }
+}
+
+function searchUser() {
+    var email_re = document.getElementById('search_email').value;
+    document.getElementById('wyszukiwanie').innerHTML = "";
+    
+    var transaction = db.transaction(["user"], "readwrite");
+    var objectStore = transaction.objectStore("user");
+    var request = objectStore.openCursor();
+
+    request.onsuccess = function(event) {
+        var cursor = event.target.result;
+        if (cursor) {
+            if (new RegExp(email_re).test(cursor.value.email)) {                
+                document.getElementById('wyszukiwanie').innerHTML += JSON.stringify(cursor.value) + "\n\n";
+            }
+
+            cursor.continue();          
+        }
+    };
+}
+
+function deleteUser() {
+    var userid = document.getElementById('delete_id').value;
+    var transaction = db.transaction(["user"], "readwrite");
+    var objectStore = transaction.objectStore("user");
+    var request = objectStore.openCursor();
+
+    request.onsuccess = function(event) {
+        var cursor = event.target.result;
+        if (cursor) {
+            console.log(cursor.value.id);           
+
+            if (userid === String(cursor.value.id)) {
+                cursor.delete();
+                loadData();
+            }
+
+            cursor.continue();          
+        }
+    };
+}
